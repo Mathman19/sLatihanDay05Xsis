aa132c6a5edd78ef36a740c909449b184b06edd6
Muhammad Akbar <akbarprima95@gmail.com>
2019-10-05 19:03:58 +0700

    day 05 xsis code

---
+++ caDay05/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caDay05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            string[] n = new string[7] { "A", "B", "C", "D", "E", "F", "G" };
+            
+            for (int i=0; i<input-1; i++)
+            {
+                for (int j = 0; j<=i && j<input; j++)
+                {
+                    Console.Write(n[i]);
+                }
+            }
+            Console.ReadKey();
+            
+        }
+    }
+}
